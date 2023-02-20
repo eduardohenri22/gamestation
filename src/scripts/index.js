6aad0a3fb8d1afe98faa879bd7138132131128e7
Eduardo.henrii <eduparaujo@outlook.com>
2023-02-19 21:34:09 -0300

    add styles, dashboard fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,21 @@
-import { profileDashboard } from "./dashboard.js";
+
 import { allGames } from "./request.js";
 
 allGames()
 
 const input = document.querySelector('input');
-const btn = document.querySelector("#btnInit");
+const btn = document.querySelector('#btnInit');
 
 function redirectPage () {
-    btn.addEventListener('click',  (e) => {
+    btn.addEventListener('click', (e) => {
         e.preventDefault()
-    
-    if(input.value) {
-        localStorage.setItem('userName', JSON.stringify(input.value));
-        window.location.replace('/src/pages/dashboard.html');
-    }
-        console.log(input.value)
+        if(input.value) {
+
+            localStorage.setItem('userName', JSON.stringify(input.value))
+            window.location.replace('/src/pages/dashboard.html')
+        }else {
+            alert('Preencha os campos!')
+        }
         
     })
 }
